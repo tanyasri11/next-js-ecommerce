25fbbbaeed623c214356f743f3050ff011f7fa0f
apna college <tsrivastava152@gmail.com>
2026-04-29 21:35:33 +0530

    fixed deploy issue

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "export",
-  images: {
-    unoptimized: true,
-  },
-  basePath: "/next-js-ecommerce",
-  assetPrefix: "/next-js-ecommerce/",
+  /* config options here */
+  
 };
 
 export default nextConfig;
